179fea7a8726aa6bc6cfed36a92c71368ab31dce
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2019-02-25 22:44:35 -0600

    cleanup and delete autogenerated

--- stutter.h
+++ stutter.h
@@ -1,3 +1,23 @@
+/*
+ * Author: Kyle Kloberdanz
+ * Date Created: 27 Nov 2018
+ * License: GNU GPLv3 (see LICENSE.txt)
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * File: stutter.h
+ */
+
+
 #ifndef STUTTER_H
 #define STUTTER_H
 
@@ -89,7 +109,7 @@ ASTNode *make_leaf_node(StutterObject *); /* just holds stutter object */
 ASTNode *make_operator_node(Operator,  /* holds operator and child items */
                             ASTNode *, /* to operate on */
                             ASTNode *);
-                                           
+
 
 
 /* destructors */
