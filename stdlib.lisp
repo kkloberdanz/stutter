4698a9dc1cc99cb92d5e01a0f444379a872f1dfc
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2019-11-02 11:09:32 -0500

    added filter to stdlib

--- stdlib.lisp
+++ stdlib.lisp
@@ -1,7 +1,13 @@
-(let (f (lambda (x) (+ 1 x))) (f 2))
-
-(let (f (lambda (x y) (+ x y))) (f 4 5))
-
 (def head (lambda (l) (index 0 l)))
-
 (def tail (lambda (l) (drop 1 l)))
+(def empty (lambda (l) (= 0 (len l))))
+
+(def rec-filter
+     (lambda (f l acc)
+       (if (empty l)
+       (acc)
+       (if (f (head l))
+         (rec-filter f (tail l) (append (head l) acc))
+         (rec-filter f (tail l) acc)))))
+
+(def filter (lambda (f l) (rec-filter f l (list))))
